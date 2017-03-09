68fae2c792a482757d102e7b760210f25fa8f619
Sewpra <sewpratv@gmail.com>
2017-03-08 22:55:45 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var today = new Date();
+var dd = today.getDate();
+//The value returned by getMonth is an integer between 0 and 11, referring 0 to January, 1 to February, and so on.
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+
+document.getElementById('post-info').innerHTML = 'Posted on ' + mm+'/'+dd+'/'+yyyy + '.';
